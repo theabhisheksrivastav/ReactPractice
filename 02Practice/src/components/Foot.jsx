46f4e30a83f97989f1da3063320bc7f73ec56803
abhisheksrivastavofficial <abhisheksrivastavofficial@gmail.com>
2024-02-14 20:30:18 +0530

    Header and footer components added

--- 02Practice/src/components/Foot.jsx
+++ 02Practice/src/components/Foot.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Foot = () => {
   return (
-    <div>Foot</div>
+    <>
+        <div>
+            <footer className="bg-amber-300 p-4 flex ">
+                <h1 className="text-2xl">Footer</h1>
+                <ul className="flex space-x-4 p-4 mx-10">
+                    <li>About</li>
+                    <li>Courses</li>
+                    <li>Contact</li>
+                </ul>
+            </footer>
+        </div>
+    </>
   )
 }
--- 02Practice/src/components/Navbar.jsx
+++ 02Practice/src/components/Navbar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <div>
+            <nav className="bg-amber-300 p-4 flex justify-between">
+                <h1 className="text-2xl">Navbar</h1>
+                <ul className="flex space-x-4">
+                    <li>About</li>
+                    <li>Courses</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    </>
   )
 }
